dbf139c7fddc1200277a45dd7269ed83c8f89f5f
Wesley Gifoni <wesleygifoni@MacBook-Pro-de-Wesley.local>
2025-08-12 18:59:51 -0700

    first commit

---
+++ src/main/java/com/finJavaAPI/FinJavaAPI/FinJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.finJavaAPI.FinJavaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinJavaApiApplication.class, args);
+	}
+
+}
